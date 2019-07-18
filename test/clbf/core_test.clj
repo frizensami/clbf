1d30217988ce38ba0447fd4e5838f7833c2c1221
Sriram Sami <frizensami@gmail.com>
2019-07-18 15:33:19 +0800

    Add mismatched brackets tests

--- test/clbf/core_test.clj
+++ test/clbf/core_test.clj
@@ -12,5 +12,16 @@
     (is (= (clbf.core/clbf-read "+") '(:add)))))
 
 (deftest reader-test-mismatched
-  (testing "Test Reader '['"
-    (is (= (clbf.core/clbf-read "[") '(:add)))))
+  (testing "Test Reader Mismatched '['"
+    (is (thrown-with-msg? Exception #"Mismatched brackets in source code." (clbf.core/clbf-read "[")))))
+
+(deftest reader-test-mismatched-close
+  (testing "Test Reader Mismatched ']'"
+    (is (thrown-with-msg? Exception #"Mismatched brackets in source code." (clbf.core/clbf-read "[")))))
+
+
+(deftest reader-test-closed-brackets
+  (testing "Test Reader '[]'"
+    (is (= (clbf.core/clbf-read "[]") '(:while :end)))))
+
+
